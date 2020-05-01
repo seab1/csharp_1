32607f10e41e3c4c189e7aadad9d3ac2ebaf2b3d
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-05-01 21:23:28 +0200

    I.gg done

--- Program.cs
+++ Program.cs
@@ -7,8 +7,16 @@ namespace SebastianSuwalaHelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Podaj liczbe calkowita:");
-            int userValue = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Twoja liczba to: {userValue}");
+
+            try
+            {
+                int userValue = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Twoja liczba to: {userValue}");
+            }
+            catch
+            {
+                Console.WriteLine("Nie podano prawidlowej wartosci!");
+            }
             Console.ReadKey();
         }
     }
