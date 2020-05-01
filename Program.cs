4ddf93424f0d13bc3bc3968bb94caed546f4ceed
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-05-01 21:39:04 +0200

    II.b done

--- Program.cs
+++ Program.cs
@@ -10,12 +10,36 @@ namespace SebastianSuwalaHelloWorld
             string number1AsString = Console.ReadLine();
             Console.WriteLine("Podaj druga liczbe:");
             string number2AsString = Console.ReadLine();
+            Console.WriteLine("Podaj znak (+, -, *, /):");
+            string sign = Console.ReadLine();
 
             try
             {
                 double number1 = double.Parse(number1AsString);
                 double number2 = double.Parse(number2AsString);
-                Console.WriteLine($"{number2} + {number2} = {number1 + number2}");
+
+                switch(sign)
+                {
+                    case "+":
+                        Console.WriteLine($"{number1} + {number2} = {number1 + number2}");
+                        break;
+
+                    case "-":
+                        Console.WriteLine($"{number1} - {number2} = {number1 - number2}");
+                        break;
+
+                    case "*":
+                        Console.WriteLine($"{number1} * {number2} = {number1 * number2}");
+                        break;
+
+                    case "/":
+                        Console.WriteLine($"{number1} / {number2} = {number1 / number2}");
+                        break;
+
+                    default:
+                        Console.WriteLine("Podany znak jest nieprawidlowy!");
+                        break;
+                }
             }
             catch
             {
