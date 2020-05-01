d8106338fe993c6a0f43b82e36019cac3f780215
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-05-01 21:15:33 +0200

    I.o done

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace SebastianSuwalaHelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
